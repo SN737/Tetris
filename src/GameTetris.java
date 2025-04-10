1d97a5bf2e5d8895c60ddec185b0d48a6120f41b
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-10 23:39:32 +0300

    Старт

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class GameTetris {
     final String TITLE = "Tetris";
     final int BLOCK_SIZE = 25;
+    final int ARC_RADIUS = 6;
 
     public static void main(String[] args) {
         new GameTetris().go();
