5cf83e67925da39945cf4bc720385eeb313c5bdc
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-10 23:50:35 +0300

    Старт

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -7,6 +7,16 @@ public class GameTetris {
     final String TITLE = "Tetris";
     final int BLOCK_SIZE = 25;
     final int ARC_RADIUS = 6;
+    final int FIELD_WIDTH = 10;
+    final int FIELD_HEIGHT = 18;
+    final int START_LOCATION = 180;
+    final int FIELD_DX = 7;
+    final int FIELD_DY = 26;
+    final int LEFT = 37;
+    final int RIGHT = 39;
+    final int UP = 38;
+    final int DOWN = 40;
+    final int SHOW_DELAY = 350;
 
     public static void main(String[] args) {
         new GameTetris().go();
