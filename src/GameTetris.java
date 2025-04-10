59c5b0ffee529fa01836575c9c121874df56dee8
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-10 23:37:47 +0300

    Старт

--- src/GameTetris.java
+++ src/GameTetris.java
@@ -4,6 +4,9 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class GameTetris {
+    final String TITLE = "Tetris";
+    final int BLOCK_SIZE = 25;
+
     public static void main(String[] args) {
         new GameTetris().go();
     }
