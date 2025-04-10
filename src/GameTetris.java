d0f76629d7a9865b0e8bf4442fd3dd78e8ee719f
Sergey <92508690+SN737@users.noreply.github.com>
2025-04-10 23:25:08 +0300

    Старт

---
+++ src/GameTetris.java
@@ -0,0 +1,13 @@
+import java.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GameTetris {
+    public static void main(String[] args) {
+        new GameTetris().go();
+    }
+    private void go() {
+
+    }
+}
